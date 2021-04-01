a4eb4bd9f04a8dab886691cdef050491f9df579b
alexayi <novikov7576@mail.ru>
2021-04-01 23:03:44 +0700

    added output of a string with the name of the shape and its coordinates in the WKT format

--- geometry.c
+++ geometry.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
+
 void ERROR(int TOF) 
 {
 	if (TOF == 0) {
@@ -11,7 +13,7 @@ void ERROR(int TOF)
 int main() {
 	char line[1000],ch, xcords[100], ycords[100];
 	int counter, radius;
-	int i = 0, j = 0, TOF = 0;
+	int i = 0, j = 0, TOF = 0, k = 0, x = 0, y = 0;
 
 	while ((ch = getchar()) != '\n') {
 		line[i] = tolower(ch);
@@ -27,48 +29,69 @@ int main() {
 		i++;
 		count++;
 	}
-	
+	while (line[i] == 32) {
+				i+=1;
+				count++;
+		}
 	// 32 - Пробел, 99 - c, 105 - i, 114 - r, 108 - l, 101 - e, 40 - (, 41 - ), 44 - , По ASCII таблице 
-	i = count - 1;
+	i = count;
 	if (line[i] == 99) { 
 		if ((line[i+1] == 105) && (line[i+2] == 114) && (line[i+3] == 99) && (line[i+4] == 108) && (line[i+5] == 101)) {
 			TOF = 1;
 			i+=6;
 			while (line[i] == 32) 
 				i++;
+			if ((line[i] != 32) && (line[i] != 40)) {
+				TOF = 0;
+				ERROR(TOF);	
+			}
 			if (line[i] == 40) {
-				i++;
-				while (isdigit(line[i]) != 0) {
-			
-					xcords[j] = line[i];
-					
-					putchar(xcords[j]);
+				i+=1;
+				while (line[i] == 32) 
+				i+=1;
+				while (isdigit(line[i])) {
+					xcords[j] = line[i];					
 					j++;
-					i++;		
-				}
-				while (isalpha(line[i] == 32)) 
 					i++;
-				if ((line[i] != 32) && (line[i] != 44)) {
+					x++;		
+			}
+			while (line[i] == 32) 
+				i+=1;
+			if ((line[i] != 32) && (line[i] != 44)) {
+				TOF = 0;
+				ERROR(TOF);	
+			}
+			if (line[i] == 44)	{
+				i+=1;
+				while (line[i] == 32) 
+				i+=1;
+				if ((line[i] != 32) && (isdigit(line[i]) == 0)) {
 					TOF = 0;
 					ERROR(TOF);	
 				}
-				if (line[i] == 44)	{
-					i+=1;
-					while (isdigit(line[i]) != 0) {
-						ycords[j] = line[i];
-						putchar(ycords[j]);
-						j++;
-						i++;		
-					}
-				}							
-			}
+				j = 0;
+				while (isdigit(line[i])) {
+					ycords[j] = line[i];
+					j++;
+					i++;
+					y++;		
+				}
+			}							
 		}
+	}
 	if (TOF == 0) 
 		ERROR(TOF);		
 	}
-	for (int k = count; k < (count + 6);k++) {
-		ch = line[k];
-		puts(ch);	
+	if (TOF != 0) {
+		for (k = count; k < (count + 6);k++)
+			putchar(line[k]);
+		printf("(");	
+		for (k = 0; k < x; k++) 
+			putchar(xcords[k]);
+		printf(", ");
+		for (k = 0; k < y; k++) 
+			putchar(ycords[k]);
+		printf(")");
 	}
 	return 0;
 }
